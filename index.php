03bb146b82d5eb28f7124083d5a7dae26c5c8311
jswc-1 <jswc@email.com>
2023-04-07 21:08:20 -0300

    daily update

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="main.css">
+        <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>        
+    </head>
+    
+    <body>
+        <!--Header section-->
+        <Div class="header">
+            <div><img id="img_logo" src="images/paddle-white.png"></div>
+            <!--div><img id="img_menu" src="images/hamburger.png"></div--->
+            <div><img id="img_menu" src="images/hamburger.png"></div>
+            <div class="header_text"><h1>Halifax Canoe and Kayak</h1></Div>
+            <Nav class="menu_naV">
+                <ul class="menu_list">
+                    <li ><a href="index.html" class="menu_item">Home</a></li>
+                    <li class="menu_item">Book a trip</li>
+                    <li class="menu_item">Admin login</li>
+                </ul>
+            </Nav>
+        </Div>
+
+        <!--Main Content-->
+        <div class="main_content">
+            <div class="main_text">
+                <Div class="Welcome"><h1>Welcome to Your IT Support System</h1></Div>
+                
+            </div>
+        </div>
+
+        <script src="script.js"></script>
+    </body>
+</html>
