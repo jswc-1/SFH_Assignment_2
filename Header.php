dca75e7a98c1cb86791f866810a22625f26cba2f
jswc-1 <jswc@email.com>
2023-04-07 21:42:12 -0300

    l20230407

---
+++ Header.php
@@ -0,0 +1,16 @@
+<!--Header section Begin-->
+<Div class="header">
+    <div><img id="img_logo" src="images/paddle-white.png"></div>
+    <!--div><img id="img_menu" src="images/hamburger.png"></div--->
+    <div><img id="img_menu" src="images/hamburger.png"></div>
+    <div class="header_text"><h1>Halifax Canoe and Kayak</h1></Div>
+    <Nav class="menu_naV">
+        <ul class="menu_list">
+            <li ><a href="index.html" class="menu_item">Home</a></li>
+            <li class="menu_item">Book a trip</li>
+            <li class="menu_item">Admin login</li>
+        </ul>
+    </Nav>
+</Div>
+<DIV class="loginButton">Login</DIV>
+<!--Header section End-->
--- index.php
+++ index.php
@@ -5,20 +5,7 @@
     </head>
     
     <body>
-        <!--Header section-->
-        <Div class="header">
-            <div><img id="img_logo" src="images/paddle-white.png"></div>
-            <!--div><img id="img_menu" src="images/hamburger.png"></div--->
-            <div><img id="img_menu" src="images/hamburger.png"></div>
-            <div class="header_text"><h1>Halifax Canoe and Kayak</h1></Div>
-            <Nav class="menu_naV">
-                <ul class="menu_list">
-                    <li ><a href="index.html" class="menu_item">Home</a></li>
-                    <li class="menu_item">Book a trip</li>
-                    <li class="menu_item">Admin login</li>
-                </ul>
-            </Nav>
-        </Div>
+        <?php include 'header.php'; ?>
 
         <!--Main Content-->
         <div class="main_content">
@@ -28,6 +15,8 @@
             </div>
         </div>
 
+
+        <?php include 'footer.php'; ?>
         <script src="script.js"></script>
     </body>
 </html>
